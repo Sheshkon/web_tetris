528e7f9ce23521a7cb9b93360418956214adc486
Alexei <sheskoalexei@gmail.com>
2022-04-05 14:05:13 +0300

    disable help button animation

--- js/main.js
+++ js/main.js
@@ -251,7 +251,7 @@ function handleTouchStart(event) {
         }
 
         if (pushedButton == 6) {
-            tetris.buttons[pushedButton].isClicked = true;
+            // tetris.buttons[pushedButton].isClicked = true;
             if (confirm("CONTROLS:\n  use screen buttons or swipes\n  - LEFT/RIGHT/DOWN swipe - left/right/down move\n  - UP swipe - rotation\n  - multitouch x3 - pause\n  - multitouch x4 - restart")) {
                 tetris.changePausedStatus();
                 // window.open("https://github.com/Sheshkon/web_tetris#web-tetris-game", '_blank');
