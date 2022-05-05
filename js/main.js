8fb2f1f570dd6b2853598935c7e67b99eaada12f
Alexei <sheskoalexei@gmail.com>
2022-05-05 19:37:57 +0300

    remove scope for serviceWorker

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ const helpMobileText = document.getElementById('mobile_help_text');
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/web_tetris/js/appCache.js', { scope: '/web_tetris/js/' })
+        .register('/web_tetris/js/appCache.js')
         .then(function() { console.log("Service Worker Registered"); });
 }
 
